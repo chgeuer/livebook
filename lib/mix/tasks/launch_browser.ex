e16f78e8430e9e2f143cebd6dfe180491823bd47
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2024-06-13 11:56:46 +0200

    Show URL

--- lib/mix/tasks/launch_browser.ex
+++ lib/mix/tasks/launch_browser.ex
@@ -2,6 +2,8 @@ defmodule Mix.Tasks.LaunchBrowser do
   use Mix.Task
 
   def run(_) do
-    System.cmd("/mnt/c/Windows/system32/cmd.exe", ["/C", "start https://beast.geuer-pollmann.de:#{LivebookWeb.Endpoint.access_struct_url.port}/?#{LivebookWeb.Endpoint.access_struct_url.query}"])
+    url = "https://beast.geuer-pollmann.de:#{LivebookWeb.Endpoint.access_struct_url.port}/?#{LivebookWeb.Endpoint.access_struct_url.query}"
+    IO.puts("Opening browser at: #{url}")
+    System.cmd("/mnt/c/Windows/system32/cmd.exe", ["/C", "start #{url}"])
   end
 end
